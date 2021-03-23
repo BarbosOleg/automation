ecd1fa0ef3c679cf6902de20a9e7e0b31ede7bb0
BarbosOleg <lolipo.celio@gmail.com>
2021-03-23 20:35:28 +0200

    ex1 commit

---
+++ atom/src/main/java/CSVWriter.java
@@ -0,0 +1,28 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class CSVWriter {
+    private static final String SAMPLE_CSV_FILE = "fl1.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE));
+
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                        .withHeader("ID", "Name", "Universitate", "Profesie","Hobby"));
+        ) {
+            csvPrinter.printRecord("1", "Andronic Roman", "USM", "student","Genshin Impact");
+            csvPrinter.printRecord("2", "Barbos Oleg", "USM", "student","Genshin Impact");
+            csvPrinter.printRecord("3", "Spinu Dan", "USM", "student","Genshin Impact");
+
+            csvPrinter.printRecord(Arrays.asList("5", "Diminet Ion", "USM", "student","Genshin Impact"));
+
+            csvPrinter.flush();
+        }
+    }
+}
---
+++ atom/src/main/java/ScannerExample.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("fl1.csv"));
+
+        //Set the delimiter used in file
+        scanner.useDelimiter(",");// \\|
+
+        while (scanner.hasNext()) {
+            System.out.print(scanner.next() + "|");
+        }
+
+        scanner.close();
+    }
+}
